74d8e85289e28202901e7bf957d6d2ac2969218c
Natnael Yohannes <natiyohannes.2017@gmail.com>
2025-07-09 18:29:50 +0300

    Add .env configuration for database connectivity
    Introduces a new .env file to manage environment variables, including NODE_ENV, PORT, and DATABASE_URL for database connection.
    
    Updates db.js to utilize the DATABASE_URL from the .env file, enhancing security and configuration management.
    
    Upgrades dotenv package to version 17.1.0 for improved functionality and security features.

--- db.js
+++ db.js
@@ -1,9 +1,19 @@
+// // db.js
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL || 'postgres://postgres:qawqaw@localhost:5432/portfolio_db'
+// });
+
+// module.exports = pool;
+
 // db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgres://postgres:qawqaw@localhost:5432/portfolio_db'
+    connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
